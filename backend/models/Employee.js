b8a05317f5a03d7088537fa9af8fce27179188ef
anshifa riya <anshifariya042@gmail.com>
2026-08-13 17:26:45 +0530

    backend updated

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -6,7 +6,6 @@ const employeeSchema = new mongoose.Schema(
       type: String,
       required: [true, "Employee name is required"],
       trim: true,
-      minlength: [2, "Name must be at least 2 characters"],
     },
 
     email: {
